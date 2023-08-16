29c08f3ff1726c88c53b820bbc920d9c4c1232bd
Matheus-A-Barboza <106258751+Matheus-A-Barboza@users.noreply.github.com>
2023-08-16 18:58:25 -0300

    Delete conversorNFs.bat

--- conversorNFs.bat
+++
@@ -1,8 +0,0 @@
-curl -o python-installer.exe https://www.python.org/ftp/python/3.10.12/python-3.10.12-amd64.exe
-python-installer.exe
-python -m pip install -U pip
-pip install pandas
-pip install openpyxl
-
-python conversorNF.py
-pause
